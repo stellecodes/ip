64b2463752d96160eb2d2a71a7de2212279eddd7
Stelle <stellelim05@gmail.com>
2025-08-20 17:19:32 +0800

    Add greeting and farewell messages

---
+++ src/main/java/SmartStelle.java
@@ -0,0 +1,11 @@
+public class SmartStelle {
+    public static void main(String[] args) {
+        String logo = "  _____      _____\n" +
+                " /          /     \n" +
+                " \\_____    \\_____ \n" +
+                "       \\         \\\n" +
+                "  _____/   _____/\n";
+        System.out.println(logo + "Hello! I am SmartStelle. What can I help you with?");
+        System.out.println("Bye, talk to you next time!");
+    }
+}

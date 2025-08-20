4acfb7c427873888f330e44736b840dea670f87e
Stelle <stellelim05@gmail.com>
2025-08-20 17:32:39 +0800

    Add echoing feature

--- src/main/java/SmartStelle.java
+++ src/main/java/SmartStelle.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class SmartStelle {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String logo = "  _____      _____\n" +
                 " /          /     \n" +
                 " \\_____    \\_____ \n" +
                 "       \\         \\\n" +
                 "  _____/   _____/\n";
         System.out.println(logo + "Hello! I am SmartStelle. What can I help you with?");
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
         System.out.println("Bye, talk to you next time!");
+
+        sc.close();
     }
 }

1475c49b31269b7dcb4d7755a48552d09ba6da6f
Stelle <stellelim05@gmail.com>
2025-08-29 17:14:29 +0800

    Change display of date to formatted

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,24 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private String start;
-    private String end;
+    private LocalDateTime start;
+    private LocalDateTime end;
 
     public Event(String start, String end, String name, boolean isCompleted) throws StelleException {
         super(name, isCompleted);
-        this.start = start;
-        this.end = end;
+        this.start = LocalDateTime.parse(start);
+        this.end = LocalDateTime.parse(end);
+    }
+
+    private String formatDate() {
+        return "from " + this.start.format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm")) + " to "
+                + this.end.format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm"));
     }
 
     @Override
     public String display() {
-        return "[E]" + super.display() + " from " + start + " to " + end;
+        return "[E]" + super.display() + " " + this.formatDate();
     }
 
     public String toSaveFormat() {

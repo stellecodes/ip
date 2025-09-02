c826a39fe5e42cab60d952582923f48b11f8f1cf
Stelle <stellelim05@gmail.com>
2025-09-02 15:24:14 +0800

    Add header comments for Storage class

--- src/main/java/smartstelle/Storage.java
+++ src/main/java/smartstelle/Storage.java
@@ -4,13 +4,36 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 
+/**
+ * Handles reading from and writing to the storage file for the SmartStelle
+ * application.
+ * <p>
+ * Provides methods to load tasks from a file into memory and save tasks from
+ * memory to a file.
+ */
 public class Storage {
     private String path;
 
+    /**
+     * Constructs a Storage object with the specified file path.
+     *
+     * @param path The file path for storing task data.
+     */
     public Storage(String path) {
         this.path = path;
     }
 
+    /**
+     * Loads tasks from the storage file into an ArrayList of Task objects.
+     * <p>
+     * If the file does not exist, it creates the file and returns an empty list.
+     * Supports loading ToDo, Deadline, and Event tasks based on the file format.
+     *
+     * @return An ArrayList containing all tasks loaded from the file.
+     * @throws IOException     If an error occurs while reading the file.
+     * @throws StelleException If there is an error creating Task objects from file
+     *                         data.
+     */
     public ArrayList<Task> load() throws IOException, StelleException {
         ArrayList<Task> tasks = new ArrayList<>();
         File file = new File(path);
@@ -44,6 +67,15 @@ public class Storage {
         return tasks;
     }
 
+    /**
+     * Saves a list of tasks to the storage file.
+     * <p>
+     * Overwrites any existing content in the file. Each task is written using
+     * its own save format defined by the toSaveFormat() method.
+     *
+     * @param tasks The list of tasks to save.
+     * @throws IOException If an error occurs while writing to the file.
+     */
     public void save(ArrayList<Task> tasks) throws IOException {
         FileWriter writer = new FileWriter(this.path);
         for (Task task : tasks) {
